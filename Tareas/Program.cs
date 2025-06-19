e42b028303ac5625645aced6eea0aa5d87215736
Luciano Torres <lucianotorreslanzavecchia@gmail.com>
2025-06-19 17:22:30 -0300

    Punto 1 y punto 2 hechos

---
+++ Tareas/Program.cs
@@ -0,0 +1,16 @@
+﻿using gestionTareas;
+using System.Text.Json;
+string url = "https://jsonplaceholder.typicode.com/todos/";
+HttpClient client = new HttpClient();
+HttpResponseMessage respuesta = await client.GetAsync(url);
+respuesta.EnsureSuccessStatusCode();
+string responseBody = await respuesta.Content.ReadAsStringAsync();
+List<Tarea> listTareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+
+foreach (var tareas in listTareas)
+{
+    Console.WriteLine($"UserId:[{tareas.userId}]");
+    Console.WriteLine($"Id de la tarea[{tareas.id}]");
+    Console.WriteLine($"Titulo:[{tareas.title}]");
+    Console.WriteLine($"Completada:[{tareas.completed}]");
+}
